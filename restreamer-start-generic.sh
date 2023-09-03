600ab0abecb1172d93d6265ca19208ee3969873e
Joseph C <neckro@gmail.com>
2023-09-02 19:03:08 -0500

    Add docker stuff

---
+++ restreamer-start-generic.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+container=restreamer
+image=datarhei/restreamer:0.6.8
+http_port=1987
+rtmp_port=1935
+password="$token"
+
+docker stop $container
+docker rm $container
+
+docker run \
+	-d \
+	--name $container \
+	-e "RS_USERNAME=admin" \
+	-e "RS_PASSWORD=$password" \
+	-e "RS_INPUTSTREAM=rtmp://localhost/live/external.stream?token=$token" \
+	-e "RS_TIMEZONE=America/Chicago" \
+	-e "RS_DEBUG=false" \
+	-e "RS_LOGLEVEL=4" \
+	-p $http_port:8080 \
+	-p $rtmp_port:1935 \
+	--restart unless-stopped \
+	$image
